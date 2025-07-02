24526016a2af39e57644aae23f8e4ad26d3090b2
Sreenand76 <sreenands93@gmail.com>
2025-07-03 00:33:45 +0530

    created and initialized frontend and backend folders

---
+++ sqlix-backend/src/test/java/com/sqlix/sqlix/SqlixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqlix.sqlix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
